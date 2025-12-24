53e1f0fe5d029cb4767486a021b833726f510c68
Danila Volkov <noxlovette@proton.me>
2025-12-24 18:22:43 +0300

    step: channel

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,49 @@
-use std::{fs::File, io::Read};
+use std::{fs::File, io::Read, sync::mpsc, thread};
 
 fn main() -> std::io::Result<()> {
-    let mut f = File::open("messages.txt")?;
-    let mut buf = [0u8; 8];
+    let f = File::open("messages.txt")?;
 
-    let mut str = String::new();
-    loop {
-        let n = f.read(&mut buf)?;
-        if n == 0 {
-            break; // EOF
-        }
+    let rx = get_lines_channel(f);
 
-        let mut chunk = &buf[..n];
-
-        if let Some(i) = chunk.iter().position(|&b| b == b'\n') {
-            str += std::str::from_utf8(&chunk[..i]).expect("utf");
-            println!("read: {str}");
-
-            chunk = &chunk[i + 1..];
-            str = String::new();
-        }
-
-        str += std::str::from_utf8(chunk).expect("utf error");
+    for line in rx {
+        println!("read: {line}");
     }
 
     Ok(())
+}
+
+fn get_lines_channel<R>(mut r: R) -> mpsc::Receiver<String>
+where
+    R: Read + Send + 'static,
+{
+    let (tx, rx) = mpsc::channel::<String>();
+    thread::spawn(move || {
+        let mut buf = [0u8; 8];
+
+        let mut str = String::new();
+        loop {
+            let n = match r.read(&mut buf) {
+                Ok(0) => break,
+                Ok(n) => n,
+                Err(e) => {
+                    print!("{e}");
+                    break;
+                }
+            };
+
+            let mut chunk = &buf[..n];
+
+            if let Some(i) = chunk.iter().position(|&b| b == b'\n') {
+                str += std::str::from_utf8(&chunk[..i]).expect("utf");
+                let _ = tx.send(str);
+
+                chunk = &chunk[i + 1..];
+                str = String::new();
+            }
+
+            str += std::str::from_utf8(chunk).expect("utf error");
+        }
+    });
+
+    rx
 }
